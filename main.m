ebf841c2769d80757141b2a2bfbd015bbb624c25
mirjamsk <mirjamskarica@gmail.com>
2016-05-18 12:13:06 +0200

    Add minimumBBArea to all setup params, and make sure violaiton deteciton works for IMG_6915_01.mat

--- main.m
+++ main.m
@@ -5,12 +5,13 @@ close all
 % tested for
 % 'video_data_sunday/IMG_5108.mov'
 % 'video_data_sunday/IMG_6919.mov'
+% 'video_data_sunday/IMG_6915_01.mov'
 
 %%
 
 %video = 'C:\\Users\\Lu\\Documents\\MATLAB\\IAProject\\video_data\\rooftop1_stabilized.avi';
 
-videoPath = 'video_data_sunday/IMG_5108.mov';
+videoPath = 'video_data_sunday/IMG_6915_01.mov';
 videoId = getVideoId(videoPath); % this is just the IMG_6919 part of the videoPath
 params = loadParameters(videoId);
 car_tracking(videoPath, params);
